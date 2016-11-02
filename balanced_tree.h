a089bf705f86ec8a79d736146b3db202a29d85d0
Levon <levon_ghukasyan@live.com>
2016-11-02 15:22:11 +0400

    Added public interface declarations for future definition

--- balanced_tree.h
+++ balanced_tree.h
@@ -2,6 +2,7 @@
 
 #include <functional>
 #include <initializer_list>
+#include <iterator>
 #include <memory>
 
 namespace std {
@@ -12,6 +13,10 @@ template <typename T,
 class balanced_tree
 {
 public:
+    using value_type = typename T;
+    using size_type = size_t;
+
+public:
     balanced_tree();
     balanced_tree(std::initializer_list<T> il);
     balanced_tree(const balanced_tree&);
@@ -20,6 +25,66 @@ public:
     balanced_tree& operator= (balanced_tree&&);
     ~balanced_tree();
 
+    // @{public interfaces
+public:
+    /*
+     * @brief insert
+     */
+    std::pair<iterator, bool> insert(const value_type& value);
+
+    /*
+     * @brief insert
+     */
+    std::pair<iterator, bool> insert(value_type&& value);
+
+    /*
+     * @brief insert
+     */
+    void insert(std::initializer_list<T> il);
+
+public:
+    /*
+     * @brief removes all data from tree
+     */
+    void clear();
+
+    /*
+     * @brief erase element from tree by position
+     */
+    iterator erase(iterator position);
+
+    /*
+     * @brief erase element from tree by position
+     */
+    iterator erase(const_iterator position);
+
+    /*
+     * @brief erase element from tree by value
+     */
+    void erase(const value_type& value);
+
+public:
+    /*
+     * @brief returns true  if tree is empty false another case
+     */
+    bool empty() const noexcept;
+
+    /*
+     * @brief returns the size of tree
+     */
+    size_type size() const noexcept;
+
+public:
+    /*
+     * @brief fine elementy by value
+     */
+    iterator find(const value_type& value) noexcept;
+
+    /*
+     * @brief fine elementy by value
+     */
+    const_iterator find(const value_type& value) const noexcept;
+
 private:
     struct bt_node
     {
@@ -27,6 +92,7 @@ private:
         bt_node* m_left_child;
         bt_node* m_right_child;
         bt_node* m_parent;
+        size_type m_height;
     };
 
     bt_node* m_head;
